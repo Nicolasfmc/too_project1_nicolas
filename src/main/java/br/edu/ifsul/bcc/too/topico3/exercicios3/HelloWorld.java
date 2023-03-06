8cb14b7dd5297a5040af562aa063331c34553f07
20202pf.cc0012 <20202pf.cc0012@pc-155978.passofundo.ifsul.local>
2023-03-06 11:32:47 -0300

    alteracao HelloWorld

--- src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios3/HelloWorld.java
+++ src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios3/HelloWorld.java
@@ -2,6 +2,6 @@ package br.edu.ifsul.bcc.too.topico3.exercicios3;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
